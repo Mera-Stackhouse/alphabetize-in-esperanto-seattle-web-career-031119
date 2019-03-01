f15e6a82814bc287f201af91df6ceeaf71f6adb7
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-03-01 05:58:50 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,26 @@
+require "pry"
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi"]
+
 def alphabetize(arr)
-  # code here
+  special = ["ĉ", "ĝ", "ĥ", "ŝ", "ŭ"]
+  list = []
+  i = 0
+  arr.each {|word|
+    list[i] = word.chars
+    i += 1
+  }
+  list2 = list.sort_by {|array|
+    array.first
+  }
+  list2
+  binding.pry
 end
+
+alphabetize(arr)
+
+
+
+# [["m", "i", " ", "a", "m", "a", "s", " ", "v", "i", "n"],
+# ["b", "o", "n", "a", "n", " ", "m", "a", "t", "e", "n", "o", "n"],
+# ["p", "a", "c", "o", "n"]]
---
+++ prac.rb
@@ -0,0 +1,15 @@
+
+
+arr = ["ad", "aa", " ", "ab", "aac", "ĝ", "c", "ĉ", "ad", "fŭ", "fu"]
+
+# new = arr.sort_by {|word|
+# word[1]
+# #word[1]
+# #puts 2
+# }
+
+word = "mera"
+
+
+
+puts arr.join
